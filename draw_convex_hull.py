95df3dd711338272936ec4a2b21e57cb744e4edc
ericpqmor <ericpqmor@gmail.com>
2019-09-02 22:56:39 -0300

    Adding gift wrapping implementation

--- draw_convex_hull.py
+++ draw_convex_hull.py
@@ -3,6 +3,9 @@ import sys
 import time
 sys.path.insert(1, 'lib')
 from draw_functions import *
+from convex_hull import *
+from polygon_generator import *
+import pylab
 
 pygame.init()
 screen_width = 700
@@ -11,14 +14,24 @@ x_margin = 70
 y_margin = 70
 
 # Reading entries from command line args
-entries = sys.argv[1:]
+# entries = sys.argv[1:]
 shapes = []
+convex_hulls = []
+x_time = []
+y_time = []
 start_time = time.time()
 
 # Getting data from entries
-for entry in entries:
-    new_shape = get_shape_from_entry(entry)
+# for entry in entries:
+for i in range(4,10):
+    new_shape = generate_convex_polygon(i)
+    # new_shape = get_shape_from_entry(entry)
     new_shape.normalize_polygon(screen_width - 2*x_margin, screen_height - 2*y_margin, x_margin, y_margin)
+    start_time = time.time()
+    convex_hulls.append(gift_wrapping_convex_hull(new_shape.points))
+    elapsed_time = 1000 * (time.time() - start_time)
+    x_time.append(i)
+    y_time.append(elapsed_time)
     shapes.append(new_shape)
 
 for i in range(len(shapes)):
@@ -26,7 +39,22 @@ for i in range(len(shapes)):
     screen = pygame.display.set_mode((screen_width, screen_height))
     screen.fill((255,255,255))
 
-    draw_points(shapes[i].points, screen, 'output/' + entries[i][5:-4] + '_convex_hull.png')
-    
-elapsed_time = time.time() - start_time
-print("Elapsed time = " + str(elapsed_time))
+    for line in convex_hulls[i]:
+        pygame.draw.line(screen, (0,0,0), line[0], line[1], 1)
+    draw_points(shapes[i].points, screen, 'output/' + str(x_time[i]) + '_convex_hull.png')
+
+# print "Here"
+
+# expected_output = []
+# for x in x_time:
+#     expected_output.append(x * x * x / 3000)
+
+# pylab.plot(x_time, y_time, 'r-', label='Brute Force')
+# pylab.plot(x_time, expected_output, 'b-', label='Expected Time')
+# pylab.xlabel('Polygon Size')
+# pylab.ylabel('Elapsed Time (ms)')
+# pylab.title
+# pylab.show()
+
+# elapsed_time = time.time() - start_time
+# print("Elapsed time = " + str(elapsed_time))
